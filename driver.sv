160756d50bdf3eb9529263a545eb1a1118dc958e
unknown <jreimer107@gmail.com>
2019-09-18 14:56:55 -0500

    rearranging into fsm's

--- driver.sv
+++ driver.sv
@@ -31,10 +31,77 @@ module driver(
 
 	reg [1:0] br_cfg_old;
 	reg [15:0] br;
-	reg [7:0] br_staging;
+	reg [7:0] databus_staging;
 	reg [1:0] br_load_cnt;
+	
+	typedef enum reg [1:0] {INIT, LD_BR_HI, LD_BR_LO, NORMAL} state_t;
+	state_t state, nxt_state;
+	
+	//State ff
+	always_ff @(posedge clk, negedge rst) begin
+		if (!rst) state <= INIT;
+		else state <= nxt_state;
+	end
+	
+	//BR_old ff
+	always_ff @(posedge clk, negedge rst) begin
+		br_cfg_old <= br_cfg;
+	end
+	
+	reg write_databus;
+	always_comb begin
+		nxt_state = INIT;
+		write_databus = 0;
+		iorw = 1;
+		
+		//Determine br from br_cfg
+		case (br_cfg)
+			2'b00:
+				br = 4800;
+				
+			2'b01:
+				br = 9600;
+				
+			2'b10:
+				br = 19200;
 
-	always@ (posedge clk, negedge rst) begin
+			2'b11:
+				br = 38400;
+
+		endcase
+	
+		case(state)
+			INIT: begin
+				nxt_state = NORMAL;
+			end
+			
+			LD_BR_HI: begin
+				databus_staging = br[15:8];
+				ioaddr = 2;
+				write_databus = 1;
+				nxt_state = LD_BR_LO;
+			end
+			
+			LD_BR_LO: begin
+				databus_staging = br[7:0];
+				ioaddr = 3;
+				write_databus = 1;
+				nxt_state = NORMAL;
+			end
+			
+			NORMAL: begin
+				ioaddr = 1;
+				if (br_cfg != br_cfg_old)
+					nxt_state = LD_BR_HI;
+				else nxt_state = NORMAL;
+			end
+			
+		endcase
+	end
+
+	
+
+/* 	always@ (posedge clk, negedge rst) begin
 		ioaddr = 2'b00;
 		iorw = 1'b1;
 		if (tbr == 1) begin
@@ -77,8 +144,8 @@ module driver(
 			br_load_cnt <= 0;
 		end
 
-	end
+	end */
 
-	assign databus = br_load_cnt != 2'b00 ? br_staging : 8'bz;
+	assign databus = write_databus ? databus_staging : 8'bz;
 
 endmodule
--- spart.sv
+++ spart.sv
@@ -38,22 +38,68 @@ module spart(
 	reg [8:0] txbuf;
 	reg [7:0] rxbuf;
 	reg [7:0] status;
-	reg [15:0] down_count;
-	wire [15:0] down_amount;
+	reg [15:0] down_count, nxt_down_count;
 
 	reg [3:0] tx_cnt;
 	assign tbr = tx_cnt == 0;
 
 	assign transmit = iorw == 0;
+	assign txd = txbuf[0];
+	
+	typedef enum reg [1:0] {INIT, WAIT, ENABLE, CHANGE} br_state_t;
+	br_state_t br_state, nxt_br_state;
+	
+	always_ff @(posedge clk, negedge rst) begin
+		if (!rst) begin
+			br_state <= INIT;
+			down_count = 50000000 / (9600 << 4) - 1;
+		end
+		else begin
+			br_state <= nxt_br_state;
+			down_count <= nxt_down_count;
+		end
+		
+	end
+	
+	always_comb begin
+		nxt_br_state = INIT;
+		nxt_down_count = down_count;
+		
+		case(br_state)
+			INIT: begin
+				//db_high = 8'h25; //Initialize baud rate to 9600
+				//db_low = 8'h80;
+				nxt_br_state = WAIT;
+			end
+			
+			CHANGE: begin
+				nxt_down_count = 50000000 / ({db_high, db_low} << 4) - 1;
+				nxt_br_state = WAIT;
+			end
+		
+			WAIT: begin
+				nxt_down_count = down_count - 1;
+				if (down_count == 0)
+					nxt_br_state = ENABLE;
+				else if (ioaddr == 2'b11)
+					nxt_br_state = CHANGE;
+				else
+					nxt_br_state = WAIT;
+			end
+			
+			ENABLE: begin
+				nxt_down_count = 50000000 / ({db_high, db_low} << 4) - 1;
+				nxt_br_state = WAIT;
+			end
+		endcase
+	end
+
 
 	always @(posedge clk, negedge rst) begin
 		if (!rst) begin
-			txbuf <= 9'h121;
-			rxbuf <= 8'h21;
+			txbuf <= 9'h1ff;
+			rxbuf <= 8'hff;
 			tx_cnt <= 4'hf;
-			db_high <= 8'hff;
-			db_low <= 8'hff;
-			down_count <= 16'h00;
 		end
 	end
 
@@ -75,27 +121,17 @@ module spart(
 			end
 			2'b10: begin
 				db_low <= databus;
-				down_count <= 0;
 			end
 			2'b11: begin
 				db_high <= databus;
-				down_count <= 0;
 			end
 		endcase
 	
-		if (enable) begin
-			down_count = {db_high, db_low};
-			txd = txbuf[0];
+		if (br_state == ENABLE) begin
 			txbuf = {1'b1, txbuf[8:1]};
 			tx_cnt = tx_cnt == 0 ? 0 : tx_cnt - 1;
 		end
-		else begin
-			down_count  = down_count < down_amount ? 0 : down_count - down_amount;
-		end
 	end
-
-	assign down_amount = 50000000 / ({db_high, db_low} << 4) - 1;
-	assign enable = down_count == 16'b0;
  
 	assign databus = transmit ? rxbuf : 8'hzz;
 
